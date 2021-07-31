fd901331a01ff9153a9d19b87f57887e43e46f57
IvanEvseenko <ivan.evseenko.2017@gmail.com>
2021-08-01 01:06:40 +0300

    ColorMix App
    Added ColorMix App

---
+++ ColorMix/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  ColorMix
+//
+//  Created by mac on 30.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        updateColor()
+        
+    }
+
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        updateColor()
+    }
+    
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSwitch.isOn {
+            red = CGFloat(redSlider.value)
+        }
+        if greenSwitch.isOn {
+            green = CGFloat(greenSlider.value)
+        }
+        if blueSwitch.isOn {
+            blue = CGFloat(blueSlider.value)
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+    }
+    
+}
+
